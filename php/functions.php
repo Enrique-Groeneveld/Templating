11304f8c77562737eabb0ad20e276d279324c8eb
Enrique Groeneveld <99062906@mydavinci.nl>
2021-05-16 23:51:50 +0200

    De bestanden toegevoegd

---
+++ php/functions.php
@@ -0,0 +1,47 @@
+<?php
+    function getDatabaseData($query) {
+        $connection = new PDO('mysql:host=localhost;dbname=characters;', 'root', 'mysql');
+
+        $data = $connection->prepare($query);
+        $data ->execute();
+        $data = $data->fetchAll();
+
+        return $data;
+    }
+
+    function getAll($table) {
+        return getDatabaseData("SELECT * FROM $table ORDER BY name");
+    }
+
+    function getOne($table, $id) {
+        return getDatabaseData("SELECT * FROM $table WHERE id = $id")[0];         
+    }
+
+    function getOtherLocations($id) {
+        return getDatabaseData("SELECT * FROM locations WHERE id != $id");
+    }
+
+    function updateLocation($location, $id) {
+        return getDatabaseData("UPDATE characters SET location = $location WHERE id = $id");
+    }
+    
+    function AddLocation($location) {
+        return getDatabaseData("INSERT INTO locations(name) VALUES ('$location')");
+    }
+
+    function deleteLocation($location) {
+        return getDatabaseData("DELETE FROM locations WHERE id = $location");
+    }
+
+    function adddefaultlocations() {
+        getDatabaseData(" UPDATE `characters` SET location = 1 WHERE id = 4;
+        UPDATE `characters` SET location = 2 WHERE id = 1;
+        UPDATE `characters` SET location = 3 WHERE id = 6;
+        UPDATE `characters` SET location = 4 WHERE id = 8;
+        UPDATE `characters` SET location = 5 WHERE id = 5 OR id = 10;
+        UPDATE `characters` SET location = 6 WHERE id = 2 OR id = 3 OR id = 9;
+        UPDATE `characters` SET location = 7 WHERE id = 7;");
+        
+
+    }
+?>
---
+++ php/locations.php
@@ -0,0 +1,70 @@
+<?php 
+    require './functions.php';
+
+    $allLocs = getAll('locations');
+    $locCount = count(getAll('locations'));
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    </head>
+    <body>
+        <header>
+            <h1>All <?=$locCount?> locations from the database.</h1> 
+            <button onclick="window.location.href='../index.php'">Go to characters</button>
+        </header>
+        <div id="container">
+            <ul>
+                <?php foreach($allLocs as $loc) { ?> 
+                    <li>
+                        <button onclick="window.location.href='./locations.php?delete=<?=$loc['id']?>'">Click me</button>
+                        <span><?=var_dump($loc['name'])?></span>
+                    </li>    
+                <?php 
+                }
+
+                if(isset($_GET['delete'])) {
+                    deleteLocation($_GET['delete']);
+                    header('Refresh:0 locations.php?success=true'); 
+                    echo '<script>alert(\'Succesfully deleted location\')</script>';
+                }
+                
+            
+            ?>    
+            </ul>     
+
+
+
+
+
+
+
+            <form method="POST">
+                <input name="location" placeholder="Put location here&hellip;"></input>
+                <button type="submit" name="submit">Add location</button>
+            </form>
+            <?php
+                if(isset($_POST['location'])){
+                    $newLoc = $_POST['location'];
+                    if(strlen($newLoc) < 1){
+                        header('Refresh:0locations.php?success=false'); 
+                        echo '<script>alert(\'Empty input\')</script>';
+                    }
+                    else {   
+                        AddLocation($newLoc);
+                        header('Refresh:0 locations.php?success=true'); 
+                        echo '<script>alert(\'Succesfully added location\')</script>'; 
+                    }
+                }
+            ?>
+        </div>
+        <footer>&copy; Ricardo Froeliger & Enrique Groeneveld 2021</footer>
+    </body>
+</html>
